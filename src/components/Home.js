2212da10a820e1edf96a267351ab1cebcaba6364
anton <Raisky34@gmail.com>
2017-06-23 11:17:36 +0300

    Create infinity scroll, create popup

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,13 +2,15 @@ import React from 'react';
 import { getPosts } from '../actions/posts';
 import PostItem from './Posts/Item';
 import { connect } from 'react-redux';
+import InfiniteScroll from './Scroller/infinityScroll';
 
 class Home extends React.Component {
 	constructor(props) {
 		super(props);
 
 		this.state = {
-			posts: []
+			posts: [],
+			hasMore: true
 		};
 	}
 
@@ -20,27 +22,50 @@ class Home extends React.Component {
 		});
 	}
 
-	render() {
-	  let postsComponent = <div>
-      Loading
-    </div>;
+	fetchData() {
+		let _this = this;
 
-	  if (this.state.posts.length !== 0) {
-      postsComponent = <div className="row">
-        {
-          this.state.posts.map((post) => {
-            return <PostItem item={post} />
-          })
-        }
-      </div>
-    }
-		return (
-        <div className="container" id="all-posts">
-          {postsComponent}
-        </div>
-		);
+		getPosts().then((response) => {
+			let newPosts = this.state.posts.concat(response);
+      _this.setState({ posts: newPosts, hasMore: false});
+		});
 	}
 
+	refresh() {
+		console.log("refresh");
+	}
+
+	render() {
+    let items = [];
+
+    this.state.posts.map((post) => {
+      items.push(<PostItem item={post}/>);
+    });
+
+    return (
+      <div className="container" id="all-posts">
+        <InfiniteScroll
+          pullDownToRefresh
+          pullDownToRefreshContent={
+            <h3 style={{textAlign: 'center'}}>&#8595; Pull down to refresh</h3>
+          }
+          releaseToRefreshContent={
+            <h3 style={{textAlign: 'center'}}>&#8593; Release to refresh</h3>
+          }
+          refreshFunction={this.refresh}
+          next={this.fetchData.bind(this)}
+          hasMore={true}
+          loader={<h4>Loading...</h4>}
+          endMessage={
+            <p style={{textAlign: 'center'}}>
+              <b>Yay! You have seen it all</b>
+            </p>
+          }>
+          {items}
+        </InfiniteScroll>
+      </div>
+    );
+  }
 }
 
 const mapStateToProps = (state) => {
