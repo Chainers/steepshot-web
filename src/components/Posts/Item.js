ece87be5684cf77bd4f83b6e00c3b02048e0fb25
anton <Raisky34@gmail.com>
2017-07-31 10:11:01 +0300

    Update modal logic, separate Item.js file

--- src/components/Posts/Item.js
+++ src/components/Posts/Item.js
@@ -8,7 +8,7 @@ import PropTypes from 'prop-types';
 import Comments from './Comments';
 import constants from '../../common/constants';
 import VouteComponent from './VouteComponent';
-import AddComment from './AddComment';
+import ItemModal from './ItemModal';
 
 class Item extends React.Component {
   constructor(props) {
@@ -27,21 +27,6 @@ class Item extends React.Component {
     this.openModal = this.openModal.bind(this);
     this.afterOpenModal = this.afterOpenModal.bind(this);
     this.closeModal = this.closeModal.bind(this);
-  }
-
-  initKeypress() {
-    const _this = this;
-
-    document.onkeydown = function(e) {
-      switch (e.keyCode) {
-        case 37:
-          _this.previous();
-          break;
-        case 39:
-          _this.next();
-          break;
-      }
-    };
   }
 
   componentDidMount() {
@@ -68,7 +53,6 @@ class Item extends React.Component {
 
   afterOpenModal() {
     this._onResize();
-    this.initKeypress();
   }
 
   closeModal() {
@@ -77,32 +61,8 @@ class Item extends React.Component {
     });
   }
 
-  next() {
-    const curIndex = this.state.currentIndex;
-    if (curIndex + 2 == this.props.items.length) {
-      this.props.loadMore();
-    }
-
-    if (curIndex == this.props.items.length) {
-      this.setState({ disableNext: true });
-    } else {
-      const newItem = this.props.items[this.state.currentIndex + 1];
-      this.resetDefaultProperties(newItem);
-      this.setState({ currentIndex: this.state.currentIndex + 1 });
-    }
-  }
-
   componentWillUnmount() {
     this.closeModal();
-  }
-
-  previous() {
-    if (this.state.currentIndex == 0) {
-      this.setState({ disablePrev: true });
-    } else {
-      this.resetDefaultProperties(this.props.items[this.state.currentIndex - 1]);
-      this.setState({ currentIndex: this.state.currentIndex - 1 });
-    }
   }
 
   resetDefaultProperties(newItem) {
@@ -135,21 +95,12 @@ class Item extends React.Component {
     });
   }
 
-  ratingVotes(event) {
-    event.stopPropagation();
-    console.log('Like.. ' + this.state.item.author + ' post');
-  }
-
   redirectToUserProfile() {
     this.setState({ redirectToReferrer: true });
   }
 
   setDefaultAvatar() {
     this.setState({ avatar: '/src/images/person.png' });
-  }
-
-  setDefaultImage() {
-    this.setState({ image: '/src/images/noimage.jpg' });
   }
 
   callPreventDefault(e) {
@@ -235,61 +186,15 @@ class Item extends React.Component {
           className='popout-container'
           contentLabel="Example Modal"
         >
-          <div id="popup" className="custom-popup">
-            <div className="my-modal">
-              <div className="">
-                <div className="popup-header">
-                  <div className="popup-title">
-                    {this.state.item.title}
-                  </div>
-                  <button type="button" className="close col-lg-1 col-md-1 col-sm-1 col-xs-1"
-                          onClick={this.closeModal}>&times;</button>
-                </div>
-                <div className="popup-body">
-                  <div className="popup-image-block" id="popup-image">
-                    <img className="popup-image" src={itemImage} onError={this.setDefaultImage.bind(this)} alt="Image"/>
-                  </div>
-                  <div className="post-popup-info" id="popup-info">
-                    <div className="author-info">
-                      <div className="">
-                        <img className="user-avatar" src={authorImage} alt="Image" onError={this.setDefaultAvatar.bind(this)} />
-                      </div>
-                      <div className="author-name">
-                        <Link to={authorLink} onClick={this.closeModal}><strong>{this.state.item.author}</strong></Link>
-                        <span>{this.state.item.about}</span>
-                      </div>
-                    </div>
-                    <br/>
-                    <div className="post-info">
-                      <VouteComponent key="vote" item={this.state.item} updateComponent={this.updateComponent.bind(this)}/>
-                      <div className="">
-                        <span className="payout-reward">{this.state.item.total_payout_reward} </span>
-                      </div>
-                    </div>
-                    <div className="hash-tags">
-                      {
-                        this.state.item.tags.map((tag, index) => {
-                          return <a key={index} onClick={(event) => _this._research(event, tag)} className="tags-urls">{tag}</a>
-                        })
-                      }
-                    </div>
-                    <div className="popup-comments">
-                      {comments}
-                    </div>
-                    <AddComment key="comment" item={this.props.item} />
-                  </div>
-                </div>
-              </div>
-            </div>
-            <div className='slick-buttons'>
-              <div className='left-button' onClick={this.previous.bind(_this)}>
-                <img className='arrow' src="/src/images/arrow_left.png" alt="Previous post"/>
-              </div>
-              <div className='right-button' onClick={this.next.bind(_this)}>
-                <img className='arrow' src="/src/images/arrow_right.png" alt="Next post"/>
-              </div>
-            </div>
-          </div>
+          <ItemModal 
+            openModal={this.openModal} 
+            closeModal={this.closeModal} 
+            item={this.props.item} 
+            items={this.props.items} 
+            index={this.props.index}
+            updateComponent={this.updateComponent.bind(this)}
+            _research={this._research}
+          />
           <ReactResizeDetector handleWidth handleHeight onResize={this._onResize.bind(this)}/>
         </Modal>
       </div>
