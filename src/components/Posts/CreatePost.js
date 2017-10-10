fa5f38e232f20cd2ad53e3fd205be6ecba3e24a1
Egor Kazachenko <eg,kazachenko@gmail.com>
2017-10-10 15:35:07 +0300

    -added: validation by quantity for tags in new post

--- src/components/Posts/CreatePost.js
+++ src/components/Posts/CreatePost.js
@@ -48,8 +48,17 @@ class CreatePost extends React.Component {
         this.setState({ [event.target.name]: event.target.value });
     }
 
+    _validateTagsByQuantity(quantity, stringWithTags) {
+        var a = stringWithTags.replace(/^\s+|\s+$/gm, '');
+        a = a.split(/\s+/);
+        return stringWithTags.replace(/^\s+|\s+$/gm, '').split(/\s+/).length <= quantity;
+    }
+
     _handleSubmit(e) {
         e.preventDefault();
+
+        if (!this._validateTagsByQuantity(4, this.state.tag.toString())) return false;
+
         const callback = (result, message) => { 
             if (result) {
                 this._getPostShaddow(message);
