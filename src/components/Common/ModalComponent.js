c2fe6ac0a01cb28b9a5f7967269a6e4372fd5f52
Egor Kazachenko <eg.kazachenko@gmail.com>
2017-10-26 14:58:02 +0300

    -hide when  pop-up is not target

--- src/components/Common/ModalComponent.js
+++ src/components/Common/ModalComponent.js
@@ -9,7 +9,7 @@ class ModalComponent extends React.Component {
             <div id="postModal" tabIndex="-1" role="dialog" aria-hidden="true" className="modal modal-post-single fade">
                 <button type="button" data-dismiss="modal" aria-hidden="true" className="close"></button>
                 <div className="modal-dialog">
-                    <div className="modal-content">
+                    <div className="modal-content js--close-if-touch">
                         {this.props.children}
                     </div>
                 </div>
