5f3cbdb6f4c9161bb12e8ea7ba331584d109ee14
pmartynov <paul.martynov@gmail.com>
2017-11-12 18:09:39 +0300

    Update guidelines

--- src/components/About/AboutComponent.js
+++ src/components/About/AboutComponent.js
@@ -16,11 +16,12 @@ class AboutComponent extends React.Component {
                     <li>Your content should not be perceived as repulsive and shocking.</li>
                     <li>Publications should not incite hatred and be directed against a social group. Also, one should not publish calls for violence or terrorism.</li>
                     <li>It is undesirable to publish something that has no attributes of authorship. For example, a screenshot of the phone screen.</li>
-                    <li>All adult content must be marked by the author by the #nsfw tag. Publication of pornographic content is not allowed and should be flagged.</li>
+                    <li>All adult content must be marked by the author by the #nsfw tag. Publication of pornographic content is not allowed.</li>
                     <li>Advertising or fraud is not allowed.</li>
                     <li>You should not publish requests for the transfer of money (for any reason) and negative media (messages in the spirit of "repay it, or it will happen terrible").</li>
                     <li>If you have doubts about your content, refrain from publishing.</li>
                 </ol>
+                <p>We expect users to downvote the content that does not follow the rules mentioned above and allow ourselves to hide it.</p>
             </div>
         </div>
     )
