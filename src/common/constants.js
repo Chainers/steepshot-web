ce73b3d16f71606eaaf8493e5ba8ccb3d128acd3
APNBHP\User <eg.kazachenko@gmail.com>
2017-11-24 13:14:57 +0300

    -correct text

--- src/common/constants.js
+++ src/common/constants.js
@@ -136,7 +136,7 @@ export default {
     COMMENT_ERROR_MESSAGE : 'You can only create comments once per 20 seconds',
     COMMENT_SUCCESS_MESSAGE : 'Comment has been successfully added',
 
-    VOTE_ACTION_WHEN_NOT_AUTH : 'Еhis action is only available to logged-in users',
+    VOTE_ACTION_WHEN_NOT_AUTH : 'This action is only available to logged-in users',
     VOTE_ACTION_WHEN_NOT_AUTH_DEBOUNCE : 500,
 
     POST_DESRIPTION_MAXLENGTH : 120,
