5a411fd210eddb41b740c5428b7685c9b01051cb
dmitryorelopt <dmitryorelopt@gmail.com>
2017-12-29 14:40:25 +0300

    added ShowIf component

---
+++ src/components/Common/ShowIf.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+class ShowIf extends React.Component {
+
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (this.props.show ?  this.props.children : null)
+  }
+}
+
+ShowIf.propTypes = {
+  show: PropTypes.bool.isRequired
+};
+
+export default ShowIf;
