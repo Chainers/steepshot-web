f84ad675c6bb70c8b8c1da55d86528551e2c056c
dmitryorelopt <dmitryorelopt@gmail.com>
2018-01-16 13:36:36 +0300

    run new component without errors

---
+++ src/components/PostsList/Post/Flag/Flag.js
@@ -0,0 +1,54 @@
+import React from 'react';
+import {connect} from 'react-redux';
+import {debounce} from 'lodash';
+import {toggleFlag} from '../../../../actions/flag';
+import Constants from '../../../../common/constants';
+
+class Flag extends React.Component {
+  constructor(props) {
+    super(props);
+    this.getOptions = this.getOptions.bind(this);
+  }
+
+  updateFlag(e) {
+    e.preventDefault();
+    this.props.toggleFlag(this.props.postIndex);
+  }
+  
+  getOptions() {
+    return this.props.postsList[Constants.POSTS_FILTERS.POSTS_HOT.point].posts[this.props.postIndex];
+  }
+  
+  render() {
+    let options = this.getOptions();
+    let buttonClasses = "btn-flag";
+    if (options.flag) {
+      buttonClasses = buttonClasses + " marked";
+    }
+    if (options.flagLoading) {
+      buttonClasses = buttonClasses + " loading";
+    }
+    
+    return (
+        <div className="wrap-btn" onClick={this.updateFlag.bind(this)}>
+          <button type="button" className={buttonClasses}></button>
+        </div>
+    );
+  }
+}
+
+const mapStateToProps = (state) => {
+  return {
+    postsList: state.postsList,
+  };
+};
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    toggleFlag: (postIndex) => {
+      dispatch(toggleFlag(postIndex));
+    }
+  }
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Flag);
--- src/reducers/postsList.js
+++ src/reducers/postsList.js
@@ -1,28 +1,36 @@
-const initialState = {
+import Constants from '../common/constants';
 
+const initialState = {
+  [Constants.POSTS_FILTERS.POSTS_HOT.point]: {
+  
+  }
 };
 
 export default function postsList(state = initialState, action) {
   switch (action.type) {
     case 'INIT_POSTS_LIST':
       return Object.assign({}, state, {
-          [action.option.point]: {
-            ...action.option
-          }
+          [action.options.point]: action.options
         });
       case 'GET_POSTS_LIST_REQUEST':
       return Object.assign({}, state, {
-          [action.option.point]: {
+          [action.point]: Object.assign({}, state[action.point], {
             loading: true
-          }
+          })
         });
     case 'GET_POSTS_LIST_SUCCESS':
+      console.log(state[action.options.point].length);
+      console.log(action.options.length);
       return Object.assign({}, state, {
-        [action.option.point]: Object.assign({}, state[action.option.point], {
-          posts: [...state[action.option.point].posts, ...action.options.posts],
+        [action.options.point]: Object.assign({}, state[action.options.point], {
+          posts: Object.assign({} ,
+            state[action.options.point].posts,
+            action.options.posts),
           offset: action.options.offset,
           hasMore: action.options.hasMore,
-          loading: false
+          loading: false,
+          length: state[action.options.point].length +
+            action.options.length
         })
       });
     default:
