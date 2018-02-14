99b5fcb58a5b47da0d6f819fab412d2c573e3990
dmitryorelopt <dmitryorelopt@gmail.com>
2018-02-14 14:47:54 +0300

    added description and error to text input component

--- src/components/Common/TextInput/TextInput.js
+++ src/components/Common/TextInput/TextInput.js
@@ -12,12 +12,14 @@ class TextInput extends React.Component {
     maxHeight: 1000,
     required: false,
     smallFont: false,
-    errorMsg: ''
+    errorMsg: '',
+    noValidCharacters: ''
   };
 
   state = {
     fontSize: this.props.smallFont ? 11 : 14,
-    fontPadding: this.props.smallFont ? 7 : 9
+    fontPadding: this.props.smallFont ? 7 : 9,
+    error: this.props.error
   };
 
   constructor(props) {
@@ -26,9 +28,14 @@ class TextInput extends React.Component {
 
   onChange(event) {
     let newValue = event.target.value;
+    let regExp = new RegExp(this.props.noValidCharacters);
+    newValue = newValue.replace(regExp, '');
     if (!this.props.multiline) {
       newValue = newValue.replace('\n', '');
       this.input.value = newValue;
+    }
+    if (this.state.error && newValue === event.target.value) {
+      this.setState({error: undefined})
     }
     this.hiddenDiv.textContent = newValue + '\n';
     this.value = newValue;
@@ -50,8 +57,9 @@ class TextInput extends React.Component {
     let prefAreaHeight = this.areaHeight;
     let paddingTop = prefAreaHeight < (minAreaHeight / 2 + 1) ? prefAreaHeight / 2 : 0;
     return (
-      <div className="container_tex-inp" onClick={() => this.input.focus()}>
-        <textarea className={'area_tex-inp input-text_tex-inp '}
+      <div className="container_tex-inp">
+        <div className="input-container_tex-inp">
+        <textarea className={'area_tex-inp input-text_tex-inp ' + (this.state.error ? 'has-error_tex-inp' : '')}
                   onChange={this.onChange.bind(this)}
                   value={this.props.value}
                   maxLength={this.props.maxLength}
@@ -63,24 +71,37 @@ class TextInput extends React.Component {
                     minHeight: minAreaHeight
                   }}
         />
-        <ShowIf show={utils.isNotEmptyString(this.props.errorMsg)}>
-          <span className="error-msg_tex-inp">{this.props.errorMsg}</span>
-        </ShowIf>
-        <label className={'title_tex-inp ' + focused}>
-          {this.props.title}
-          <ShowIf show={this.props.required}>
-            <span className="required_tex-inp"> *</span>
-          </ShowIf>
-        </label>
-        <div className="hidden-div_tex-inp input-text_tex-inp"
-             ref={ref => this.hiddenDiv = ref}
-             style={{
-               fontSize: this.state.fontSize + 'px',
-               lineHeight: (this.state.fontSize + this.state.fontPadding) + 'px'
-             }}
-        >
-          {this.props.value || ''}
+          <label className={'title_tex-inp ' + focused} onClick={() => this.input.focus()}>
+            {this.props.title}
+            <ShowIf show={this.props.required}>
+              <span className="required_tex-inp"> *</span>
+            </ShowIf>
+          </label>
+          <div className="hidden-div_tex-inp input-text_tex-inp"
+               ref={ref => this.hiddenDiv = ref}
+               style={{
+                 fontSize: this.state.fontSize + 'px',
+                 lineHeight: (this.state.fontSize + this.state.fontPadding) + 'px'
+               }}
+          >
+            {this.props.value || ''}
+          </div>
         </div>
+        <ShowIf show={this.props.children}>
+          <div className="children_tex-inp">
+            {this.props.children}
+          </div>
+        </ShowIf>
+        <ShowIf show={this.state.error}>
+          <div className="error_tex-inp">
+            {this.state.error}
+          </div>
+        </ShowIf>
+        <ShowIf show={this.props.description}>
+          <div className="description_tex-inp">
+            {this.props.description}
+          </div>
+        </ShowIf>
       </div>
     );
   }
