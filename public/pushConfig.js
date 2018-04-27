502b9ff4baa0b5bbe2b2889c003f0cd23dea164e
dmitryorelopt <dmitryorelopt@gmail.com>
2018-04-27 12:42:38 +0300

    fixed config

--- public/pushConfig.js
+++ public/pushConfig.js
@@ -1,23 +1,10 @@
 var OneSignal = window.OneSignal || [];
-OneSignal.push(["init", {
-	appId: "77fa644f-3280-4e87-9f14-1f0c7ddf8ca5",
-	autoRegister: false,
-	httpPermissionRequest: {
-		enable: true
-	},
-	subdomainName: 'qa-second',
-	/*welcomeNotification: {
-		"title": "My Custom Title",
-		"message": "Thanks for subscribing!",
-		"url": "https://qa-second.alpha.steepshot.io/browse"
-	},
-	promptOptions: {
-		actionMessage: "We'd like to show you notifications for the latest news and updates.",
-		acceptButtonText: "ALLOW",
-		cancelButtonText: "NO THANKS"
-	}*/
-}]);
-/*
-OneSignal.push(function () {
-	OneSignal.showHttpPrompt();
-});*/
+OneSignal.push(function() {
+	OneSignal.init({
+		appId: "77fa644f-3280-4e87-9f14-1f0c7ddf8ca5",
+		autoRegister: false,
+		notifyButton: {
+			enable: true,
+		},
+	});
+});
