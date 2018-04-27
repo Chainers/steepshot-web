c25d0003dc8d166f43c895494a8ef77d04693242
dmitryorelopt <dmitryorelopt@gmail.com>
2018-04-27 11:55:23 +0300

    removed all config

--- public/pushConfig.js
+++ public/pushConfig.js
@@ -4,17 +4,17 @@ OneSignal.push(["init", {
 	autoRegister: false,
 	notifyButton: {
 		enable: true, /* Required to use the Subscription Bell */
-		/* SUBSCRIPTION BELL CUSTOMIZATIONS START HERE */
-		size: 'medium', /* One of 'small', 'medium', or 'large' */
-		theme: 'default', /* One of 'default' (red-white) or 'inverse" (white-red) */
-		position: 'bottom-right', /* Either 'bottom-left' or 'bottom-right' */
+	/*	/!* SUBSCRIPTION BELL CUSTOMIZATIONS START HERE *!/
+		size: 'medium', /!* One of 'small', 'medium', or 'large' *!/
+		theme: 'default', /!* One of 'default' (red-white) or 'inverse" (white-red) *!/
+		position: 'bottom-right', /!* Either 'bottom-left' or 'bottom-right' *!/
 		offset: {
 			bottom: '0px',
-			left: '0px', /* Only applied if bottom-left */
-			right: '0px' /* Only applied if bottom-right */
+			left: '0px', /!* Only applied if bottom-left *!/
+			right: '0px' /!* Only applied if bottom-right *!/
 		},
-		prenotify: true, /* Show an icon with 1 unread message for first-time site visitors */
-		showCredit: false, /* Hide the OneSignal logo */
+		prenotify: true, /!* Show an icon with 1 unread message for first-time site visitors *!/
+		showCredit: false, /!* Hide the OneSignal logo *!/
 		text: {
 			'tip.state.unsubscribed': 'Subscribe to notifications',
 			'tip.state.subscribed': "You're subscribed to notifications",
@@ -41,28 +41,29 @@ OneSignal.push(["init", {
 			'dialog.button.background': 'rgb(84,110,123)',
 			'dialog.button.foreground': 'white'
 		},
-		/*/!* HIDE SUBSCRIPTION BELL WHEN USER SUBSCRIBED *!/
+		/!*!/!* HIDE SUBSCRIPTION BELL WHEN USER SUBSCRIBED *!/
 		displayPredicate: function () {
 			return OneSignal.isPushNotificationsEnabled()
 				.then(function (isPushEnabled) {
 					return !isPushEnabled;
 				});
-		}*/
+		}*!/
 	},
 	welcomeNotification: {
 		"title": "My Custom Title",
 		"message": "Thanks for subscribing!",
-		// "url": "" /* Leave commented for the notification to not open a window on Chrome and Firefox (on Safari, it opens to your webpage) */
+		// "url": "" /!* Leave commented for the notification to not open a window on Chrome and Firefox (on Safari, it opens to your webpage) *!/
 	},
 	promptOptions: {
-		/* actionMessage limited to 90 characters */
+		/!* actionMessage limited to 90 characters *!/
 		actionMessage: "We'd like to show you notifications for the latest news and updates.",
-		/* acceptButtonText limited to 15 characters */
+		/!* acceptButtonText limited to 15 characters *!/
 		acceptButtonText: "ALLOW",
-		/* cancelButtonText limited to 15 characters */
+		/!* cancelButtonText limited to 15 characters *!/
 		cancelButtonText: "NO THANKS"
-	}
+	}*/
 }]);
+/*
 OneSignal.push(function () {
 	OneSignal.showHttpPrompt();
-});
+});*/
